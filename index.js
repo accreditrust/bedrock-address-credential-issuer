9c76930af3b69d1ebbdf10224e1920a520fecf30
OmarMalik <omaramalik94@gmail.com>
2015-03-16 12:51:09 -0400

    Set up file structure for project.

---
+++ index.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
+ */
+var bedrock = require('bedrock');
+var av = require('./lib/addressCredentialIssuer');
+
+// run with development defaults
+bedrock.start();
