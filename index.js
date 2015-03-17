8c62fa4b0fee9a433c05c8d82e2ea52842645c5b
OmarMalik <omaramalik94@gmail.com>
2015-03-17 01:41:21 -0400

    Set up mocha tests.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2012-2015 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
-var av = require('./lib/addressCredentialIssuer');
+var aci = require('./lib/addressCredentialIssuer');
 
 // run with development defaults
 bedrock.start();
