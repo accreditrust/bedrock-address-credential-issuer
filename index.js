1e669b53b4d4a84ef87c8ee2ee43d7b1c1920309
Omar Malik <omalik@vera.digitalbazaar.com>
2015-03-30 18:00:20 -0400

    Add views directory to views.paths.

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
  */
 var bedrock = require('bedrock');
 var aci = require('./lib/addressCredentialIssuer');
+var path = require('path');
+
+require('bedrock-idp');
+require('bedrock-views');
+
+bedrock.config.views.paths.push(
+  path.join(__dirname, 'views')
+);
 
 // run with development defaults
 bedrock.start();
